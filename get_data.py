a90a2a629b57e848edaa89921aa98c7f18464bb2
Sridaran Thoniyil <sri7thon@gmail.com>
2019-12-01 19:05:22 -0500

    Write initial documentation for external Repl API

--- get_data.py
+++ get_data.py
@@ -14,6 +14,72 @@ class InteractiveRepl:
     """
     def __init__(self, config):
         self.config = config
+
+    """
+    ( ) in commands denote optional letters
+    ( ) in other contexts denotes comments
+    
+    < > denotes an argument
+    
+    Tabbing denotes subcommands a/o related comments
+
+    | (single) at the beginning of a line denotes a group
+        Groups can be broken
+    
+    Double tabbing OR
+    Single tabbed groups
+        denote subcommands a/o related comments that apply
+        to the last (latest) group
+    
+    || (double) surrounding a word denotes a keyword non-positional argument
+        These must come after the base command, but order does not matter
+    
+    * at the beginning of a line means that the command creates a subcontext
+        Exit out from subcontext with 'exit'
+        Preface input with 'global' to issue commands from the global context
+            This will usually exit out from subcontext by default
+
+    command list
+        comp(etition) <sku>
+        team <team name>
+        config
+            set <key> <value>
+                (throws error if invalid type)
+            get <key>
+                (throws error if invalid key)
+            update
+                (updates config file if anything changed)
+        match (default next)
+            load
+                (if team name supplied)
+                    (load organization's match list)
+                        (!! CONFIGURATION: organization or team)
+            | * next
+            | * prev
+                    (if team name supplied)
+                        (return team's last played match)
+                    (else)
+                        (return last updated match)
+            * lookup <n>
+                (n would be nth match)
+            wait
+            history
+                (returns team's record; match history)
+                        team <name>
+        stats (default [current_team])
+            || comp || (default)
+            || world ||
+            | * <team name>
+            | * <organization name>
+                (loads stats)
+                    (750B vs 750)
+            rank
+                    <team name>
+        * history <team name>
+            (<team name> defaults to our team)
+            (returns match history of given team)
+
+    """
     def eval(self, string):
         print(string[::-1])
     def lookup(self, key):
